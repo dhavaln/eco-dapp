4b76dbc147c13f74db56ae74bb4fecce416287c1
dhaval nagar <dhavalnagar@dhavals-MacBook-Air.local>
2022-05-18 16:52:54 +0530

    limit the totalsupply based on given value

--- contracts/CompanyERC.sol
+++ contracts/CompanyERC.sol
@@ -7,6 +7,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract CompanyERC is ERC20 {
 
     constructor (address tokenOwner, string memory companyName, string memory tokenName, uint256 totalSupply) ERC20(companyName, tokenName) {
-        _mint(tokenOwner, totalSupply * (10 ** uint256(decimals())));
+        _mint(tokenOwner, totalSupply);
     }
 }
