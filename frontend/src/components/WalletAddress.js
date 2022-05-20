fc9b12baaba081ecb224e45e0ed1bd83cf08eb3b
dhaval nagar <dhavalnagar@dhavals-MacBook-Air.local>
2022-05-20 23:48:12 +0530

    address copy icon added

--- frontend/src/components/WalletAddress.js
+++ frontend/src/components/WalletAddress.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { FaRegCopy } from 'react-icons/fa';
 
 export function WalletAddress({ label, address }) {
   return (
     <span>
-      { label }: <b>...{address.substr(address.length - 5)}</b>
+      { label }: <b>{address.substr(0, 5)}...{address.substr(address.length - 4)} <FaRegCopy onClick={() => navigator.clipboard.writeText(address)}/></b>
     </span>
   );
 }
